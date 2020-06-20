0528f16965bf62bbe3489a77d75557a43e9fbe32
mbrad26 <mbrad26@gmail.com>
2020-06-20 10:00:00 +0100

    Implemented the first test for the App component

---
+++ src/components/App/App.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+
+describe('App', () => {
+  it('should render a <div>', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('div.app-container').length).toEqual(1);
+  });
+});
